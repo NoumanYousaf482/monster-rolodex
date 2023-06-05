f43780739d6cafe84ead530b50bca030b52ae1d2
NoumanYousaf482 <m.noumanyousaf@gmail.com>
2023-06-05 11:09:12 +0500

    027 Monsters Rolodex - Input Search Box Component

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
     this.state={
       monster:[],
     };
+    console.log('constructor');
   }
 
   componentDidMount(){
+    console.log('componenet did mount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response)=> response.json())
     .then((users)=> this.setState(() => {
@@ -26,8 +28,17 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
     return (
       <div className='App'>
+        <input 
+        className='search-box' 
+        type='search' 
+        placeholder='search monster'
+        onChange={(event) => {
+          console.log(event.target.value);
+        }}
+         />
         {this.state.monster.map((monster) =>{
           return (
             <div key={monster.id}>
